031d884cc0af6da6ea14b891e4150d3f4eb45824
Nick Miyake <nmiyake@users.noreply.github.com>
2018-03-24 21:52:30 -0700

    Implement configuration upgrades (#5)

--- importalias/importalias.go
+++ importalias/importalias.go
@@ -15,7 +15,6 @@
 package importalias
 
 import (
-	"github.com/palantir/okgo/checker"
 	"github.com/palantir/okgo/okgo"
 )
 
@@ -23,13 +22,3 @@ const (
 	TypeName okgo.CheckerType     = "importalias"
 	Priority okgo.CheckerPriority = 0
 )
-
-func Creator() checker.Creator {
-	return checker.NewCreator(
-		TypeName,
-		Priority,
-		func(cfgYML []byte) (okgo.Checker, error) {
-			return checker.NewAmalgomatedChecker(TypeName, checker.Priority(Priority)), nil
-		},
-	)
-}
